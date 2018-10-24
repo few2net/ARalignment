f4bb90d5d8d1d45b4a35782efa09dd1d42fad736
few2net <tanyatep.tot@gmail.com>
2018-10-24 17:33:12 +1100

    import asset
    - create script
    - setup Controller
    *import from previous project

---
+++ ARalignment/Assets/Resource/Scripts/Controller.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Controller for AugmentedImage.
+/// </summary>
+/// 
+
+public class Controller : MonoBehaviour
+{
+
+    public EnomotoVisualizer VisualizePrefab;
+    public GameObject FitToScanOverlay;
+
+    private Dictionary<int, EnomotoVisualizer> m_Visualizers
+            = new Dictionary<int, EnomotoVisualizer>();
+
+    private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Check that motion tracking is tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            return;
+        }
+
+        // Get updated augmented images for this frame.
+        Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
+
+        // Create visualizers and anchors for updated augmented images that are tracking and do not previously
+        // have a visualizer. Remove visualizers for stopped images.
+        foreach (var image in m_TempAugmentedImages)
+        {
+            EnomotoVisualizer visualizer = null;
+            m_Visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+            if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+            {
+                // Create an anchor to ensure that ARCore keeps tracking this augmented image.
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                visualizer = (EnomotoVisualizer)Instantiate(VisualizePrefab, anchor.transform);
+                visualizer.Image = image;
+                m_Visualizers.Add(image.DatabaseIndex, visualizer);
+            }
+            else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+            {
+                m_Visualizers.Remove(image.DatabaseIndex);
+                GameObject.Destroy(visualizer.gameObject);
+            }
+        }
+
+        // Show the fit-to-scan overlay if there are no images that are Tracking.
+        foreach (var visualizer in m_Visualizers.Values)
+        {
+            if (visualizer.Image.TrackingState == TrackingState.Tracking)
+            {
+                FitToScanOverlay.SetActive(false);
+                return;
+            }
+        }
+
+        FitToScanOverlay.SetActive(true);
+    }
+}
+
---
+++ ARalignment/Assets/Resource/Scripts/EnomotoVisualizer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using GoogleARCore;
+using GoogleARCoreInternal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnomotoVisualizer : MonoBehaviour
+{
+
+    /// <summary>
+    /// The AugmentedImage to visualize.
+    /// </summary>
+    public AugmentedImage Image;
+
+    public GameObject Enomoto;
+
+
+    // refer to center point of an image
+    public float offsetX = 0; // width (+right -left)
+    public float offsetZ = 0; // height (+up -down)
+    public float offsetY = 0; // deep (+in -out)
+
+    public void Update()
+    {
+        if (Image == null || Image.TrackingState != TrackingState.Tracking)
+        {
+            Enomoto.SetActive(false);
+            return;
+        }
+
+
+        if (offsetX != 0 || offsetZ != 0 || offsetY != 0)
+        {
+            Enomoto.transform.localPosition = (offsetX * Vector3.left) + (offsetZ * Vector3.back) + (offsetY * Vector3.up);
+        }
+
+        // Enomoto.transform.localPosition = (halfWidth * Vector3.left)+ (halfHeight * Vector3.back) * 21 +
+        //      (Vector3.up);
+        Enomoto.SetActive(true);
+
+        var sizeX = GameObject.Find("Canvas/sizeX").GetComponent<Text>();
+        sizeX.text = "X : " + Image.ExtentX.ToString();
+        var sizeZ = GameObject.Find("Canvas/sizeZ").GetComponent<Text>();
+        sizeZ.text = "Z : " + Image.ExtentZ.ToString();
+        var center = GameObject.Find("Canvas/center").GetComponent<Text>();
+        center.text = "cal : " + ((float)1.9 * Vector3.back);
+        //center.text = "C : " + Image.CenterPose.ToString();
+    }
+}
