6ed65d456664aae1dfa93fcbdbf0e0ecb8493d09
few2net <tanyatep.tot@gmail.com>
2018-11-03 17:04:42 +1100

    Create raycaster (1)
    - duplicate scripts of plane and pointcloud
    - create cursor ray
    - testing

---
+++ ARalignment/Assets/Resource/Scripts/CursorRay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class CursorRay : MonoBehaviour {
+
+
+    public Camera FirstPersonCamera;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Raycast against the location the player touched to search for planes.
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+            TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+        if (Frame.Raycast(Screen.width/2, Screen.height/2, raycastFilter, out hit))
+        {
+            // Use hit pose and camera pose to check if hittest is from the
+            // back of the plane, if it is, no need to create the anchor.
+            if ((hit.Trackable is DetectedPlane) &&
+                Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                    hit.Pose.rotation * Vector3.up) < 0)
+            {
+                Debug.Log("Hit at back of the current DetectedPlane");
+            }
+            else
+            {
+                //Destroy(hit.Trackable);
+                print("detected!");
+                print(hit.Flags);
+            }
+        }
+    }
+}
