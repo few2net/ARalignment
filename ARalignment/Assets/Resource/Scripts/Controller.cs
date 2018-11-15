3e72ef242076943ecb4d63a28796ed1a6829e47c
few2net <tanyatep.tot@gmail.com>
2018-11-15 18:20:39 +1100

    create new ui
    - create find plane canvas
    - create rotation button
    - change model offset

--- ARalignment/Assets/Resource/Scripts/Controller.cs
+++ ARalignment/Assets/Resource/Scripts/Controller.cs
@@ -13,11 +13,14 @@ public class Controller : MonoBehaviour
 {
 
     public EnomotoVisualizer VisualizePrefab;
+    public GameObject Canvas0;
+    public GameObject Canvas1;
     public GameObject FitToScanOverlay;
-
+    
     private Dictionary<int, EnomotoVisualizer> m_Visualizers
             = new Dictionary<int, EnomotoVisualizer>();
 
+    private List<DetectedPlane> m_AllPlanes = new List<DetectedPlane>();
     private List<AugmentedImage> m_TempAugmentedImages = new List<AugmentedImage>();
 
     // Update is called once per frame
@@ -35,10 +38,23 @@ public class Controller : MonoBehaviour
             return;
         }
 
+        // Hide snackbar when currently tracking at least one plane.
+        Session.GetTrackables<DetectedPlane>(m_AllPlanes);
+        bool showSearchingUI = true;
+        for (int i = 0; i < m_AllPlanes.Count; i++)
+        {
+            if (m_AllPlanes[i].TrackingState == TrackingState.Tracking)
+            {
+                showSearchingUI = false;
+                break;
+            }
+        }
+        Canvas0.SetActive(showSearchingUI);
+        Canvas1.SetActive(!showSearchingUI);
+
         // Get updated augmented images for this frame.
         Session.GetTrackables<AugmentedImage>(m_TempAugmentedImages, TrackableQueryFilter.Updated);
         
-
         // Create visualizers and anchors for updated augmented images that are tracking and do not previously
         // have a visualizer. Remove visualizers for stopped images.
         foreach (var image in m_TempAugmentedImages)
@@ -76,19 +92,34 @@ public class Controller : MonoBehaviour
     /// <summary>
     /// button code
     /// </summary>
-
-    private int mode = 88;
+    
+    private int mode = 0;
+    private int axis = 0;
+    private string[] mode_text = {"Pos", "Rot"};
+    private string[] axis_text = { "X", "Y", "Z" };
 
     public void changeMode()
     {
-        if (mode == 90)
+        if (mode == 1)
         {
-            mode -= 3;
+            mode -= 2;
         }
         mode += 1;
 
         var mode_btn = GameObject.Find("Canvas/mode_btn/Text").GetComponent<Text>();
-        mode_btn.text = "" + (char)mode;
+        mode_btn.text = mode_text[mode];
+    }
+
+    public void changeAxis()
+    {
+        if (axis == 2)
+        {
+            axis -= 3;
+        }
+        axis += 1;
+
+        var axis_btn = GameObject.Find("Canvas/axis_btn/Text").GetComponent<Text>();
+        axis_btn.text = axis_text[axis];
     }
 
     public void increase()
@@ -96,19 +127,29 @@ public class Controller : MonoBehaviour
         EnomotoVisualizer Enomoto = null;
         m_Visualizers.TryGetValue(0, out Enomoto);
 
-        if (mode == 88)
+        if (mode == 0 && axis == 0)
         {
             Enomoto.offsetX += 0.005f;
         }
-
-        else if (mode == 89)
+        else if (mode == 0 && axis == 1)
         {
             Enomoto.offsetY += 0.005f;
         }
-
-        else if (mode == 90)
+        else if (mode == 0 && axis == 2)
         {
             Enomoto.offsetZ += 0.005f;
+        }
+        else if (mode == 1 && axis == 0)
+        {
+            Enomoto.transform.Rotate(Vector3.right);
+        }
+        else if (mode == 1 && axis == 1)
+        {
+            Enomoto.transform.Rotate(Vector3.up);
+        }
+        else if (mode == 1 && axis == 2)
+        {
+            Enomoto.transform.Rotate(Vector3.forward);
         }
     }
 
@@ -117,19 +158,29 @@ public class Controller : MonoBehaviour
         EnomotoVisualizer Enomoto = null;
         m_Visualizers.TryGetValue(0, out Enomoto);
 
-        if (mode == 88)
+        if (mode == 0 && axis == 0)
         {
             Enomoto.offsetX -= 0.005f;
         }
-
-        else if (mode == 89)
+        else if (mode == 0 && axis == 1)
         {
             Enomoto.offsetY -= 0.005f;
         }
-
-        else if (mode == 90)
+        else if (mode == 0 && axis == 2)
         {
             Enomoto.offsetZ -= 0.005f;
+        }
+        else if (mode == 1 && axis == 0)
+        {
+            Enomoto.transform.Rotate(Vector3.left);
+        }
+        else if (mode == 1 && axis == 1)
+        {
+            Enomoto.transform.Rotate(Vector3.down);
+        }
+        else if (mode == 1 && axis == 2)
+        {
+            Enomoto.transform.Rotate(Vector3.back);
         }
     }
 
