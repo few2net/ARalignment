0402d05bdf31c9a5a1a7b7f235d8687575c1f94d
few2net <tanyatep.tot@gmail.com>
2018-11-23 15:08:18 +1100

    Create new canvas
    - create canvas1
    - copy arcore script
    - import asset

--- ARalignment/Assets/Resource/Scripts/PointVisualizer.cs
+++ ARalignment/Assets/Resource/Scripts/PointVisualizer.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SearchPlaneUI : MonoBehaviour {
+public class PointVisualizer : MonoBehaviour {
 
-    public GameObject SearchingForPlaneUI;
-
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		
 	}
 	
